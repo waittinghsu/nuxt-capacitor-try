cde0040029045e8d20280e62fbc120dadcb2b493
waitting <spidergod23@gamil.com>
2025-02-23 19:51:08 +0800

    init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'example.omega.com',
-  appName: 'nuxt-app',
-  webDir:  'dist', // Nuxt 構建生成目錄
+    appId: 'example.omega.com',
+    appName: 'nuxt-app',
+    webDir: 'dist', // Nuxt 構建生成目錄
 };
 
 export default config;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false, // 關閉 SSR（不啟用服務端渲染）
+  nitro: {
+    preset: 'static' // 替代原來的 target: 'static'
+  },
+  devServer: {
+    port: 6969 // 推荐使用 devServer
+  },
   // 確保啟用 Vite
   vite: {
     // 如果需要自定義配置，可以在這裡添加
+    optimizeDeps: {
+      include: ['vue', 'vue-router']
+    }
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
