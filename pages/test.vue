8215908ab2700754771d88bf3d56234daeb7ad7c
spidergod <spidergod23@gmail.com>
2025-02-25 17:22:02 +0800

    Create test.vue

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+console.log(Button, Card);
+
+</script>
+
+<template>
+  <Card>
+    <Button></Button>
+  </Card>
+</template>
+
+<style scoped>
+
+</style>
