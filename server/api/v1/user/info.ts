17b986df52d7f84f8fb4dfcd9db835cc1ffc60d2
spidergod <spidergod23@gmail.com>
2025-02-27 16:03:54 +0800

    Create info.ts

---
+++ server/api/v1/user/info.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler(() => {
+    return {
+        code: 200,
+        result: {
+            name: 'omega',
+            id: 1,
+            group: 'DDT',
+            menu: [
+                { id: 1, name: 'Dashboard', key: 'dashboard' },
+                { id: 2, name: 'About', key: 'about' },
+                { id: 3, name: 'UserInfo', key: 'userInfo' },
+            ],
+            role: 'admin'
+        }
+    }
+})
