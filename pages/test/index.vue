ff7d88dd44f9508a1a6059a7932c90d52d203438
spidergod <spidergod23@gmail.com>
2025-03-04 15:43:28 +0800

    Create index.vue

---
+++ pages/test/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import SectionArea from "./components/sectionArea.vue";
+
+</script>
+
+<template>
+  <div>
+    <section-area></section-area>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
