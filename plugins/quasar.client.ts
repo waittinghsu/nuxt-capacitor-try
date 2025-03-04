66d95bc57b6d04df7aa8979ce350092d2bbcdb86
spidergod <spidergod23@gmail.com>
2025-03-04 16:49:59 +0800

    Update quasar.client.ts

--- plugins/quasar.client.ts
+++ plugins/quasar.client.ts
@@ -1,20 +1,41 @@
-import { Quasar, Notify, QBtn, QCard, QChip, QCardSection, QCardActions, QImg, QInput } from 'quasar'
-import 'quasar/dist/quasar.css'
-import '@quasar/extras/material-icons/material-icons.css'
+import { Notify, QAvatar, QBtn, QCard, QCardActions, QCardSection, QChip, QDrawer, QFooter, QHeader, QImg, QInput, QItem, QItemSection, QLayout, QList, QPageContainer, QScrollArea, QSeparator, QTab, QTabs, QToolbar, QToolbarTitle, Quasar, Ripple } from 'quasar';
+import 'quasar/dist/quasar.css';
+import '@quasar/extras/material-icons/material-icons.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(Quasar, {
+        directives: {
+            Ripple, // 显式注册 Ripple 指令
+        },
         plugins: {
             Notify,
         },
+        config: {
+            dark: true, // 全局默认暗黑模式
+        },
         components: {
+            QAvatar,
             QBtn,
             QCard,
             QChip,
             QCardSection,
             QCardActions,
+            QDrawer,
+            QFooter,
+            QHeader,
             QImg,
+            QItem,
+            QItemSection,
             QInput,
+            QLayout,
+            QList,
+            QPageContainer,
+            QScrollArea,
+            QSeparator,
+            QTabs,
+            QTab,
+            QToolbar,
+            QToolbarTitle,
         },
-    })
-})
+    });
+});
