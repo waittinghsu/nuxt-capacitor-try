907f3921d87356d39a0d0c678e8d483a13d2db2a
spidergod <spidergod23@gmail.com>
2025-03-11 18:14:07 +0800

    Create route-enhancer.ts

---
+++ plugins/route-enhancer.ts
@@ -0,0 +1,17 @@
+import { useRouteEnhancerStore } from '@/stores/useRouteEnhancerStore';
+import { useRouter } from 'vue-router';
+
+export default defineNuxtPlugin(() => {
+    const router = useRouter();
+    const enhancerStore = useRouteEnhancerStore();
+
+    // 在路由初始化後，遍歷並處理所有路由資料
+    router.getRoutes().forEach((route) => {
+        const newRoute = enhancerStore.processRoute(route);
+        // 根據需求，若路由有 name 就移除原路由並新增帶加工後的路由
+        if (typeof route.name === 'string') {
+            router.removeRoute(route.name);
+            router.addRoute(newRoute);
+        }
+    });
+});
