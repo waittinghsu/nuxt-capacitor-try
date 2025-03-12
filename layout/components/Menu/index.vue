640d7aba9ac10277cd8fd4608d296aeb50b6f0b3
spidergod <spidergod23@gmail.com>
2025-03-12 18:37:37 +0800

    Update index.vue

--- layout/components/Menu/index.vue
+++ layout/components/Menu/index.vue
@@ -2,13 +2,14 @@
 import type { AppMenuItem } from '@/stores/useMenu';
 import MenuItem from './MenuItem.vue';
 
-const { menu } = defineProps<{
+const { menu, level } = defineProps<{
     menu: AppMenuItem[]
+    level: number
 }>();
 </script>
 
 <template>
     <q-list>
-        <MenuItem v-for="item in menu" :key="item.key" :menu="item" />
+        <MenuItem v-for="item in menu" :key="item.key" :menu="item" :level="level" />
     </q-list>
 </template>
