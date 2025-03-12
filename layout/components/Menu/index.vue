e1f56cf2e1492be01aa3a1a43e8fbe4461dffd3b
spidergod <spidergod23@gmail.com>
2025-03-12 18:12:04 +0800

    Update and rename index.vue to index.vue

---
+++ layout/components/Menu/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { AppMenuItem } from '@/stores/useMenu';
+import MenuItem from './MenuItem.vue';
+
+const { menu } = defineProps<{
+    menu: AppMenuItem[]
+}>();
+</script>
+
+<template>
+    <q-list>
+        <MenuItem v-for="item in menu" :key="item.key" :menu="item" />
+    </q-list>
+</template>
