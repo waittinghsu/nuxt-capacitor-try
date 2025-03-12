64ba371baff119969f2661658c36ba66a5823e3a
spidergod <spidergod23@gmail.com>
2025-03-12 18:13:03 +0800

    Create MenuItem.vue

---
+++ layout/components/Menu/MenuItem.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { AppMenuItem } from '@/stores/useMenu';
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import MenuItem from './MenuItem.vue'; // 這樣可以遞迴使用
+
+const props = defineProps<{
+    menu: AppMenuItem
+}>();
+
+const router = useRouter();
+
+const hasChildren = computed(() => {
+    return props.menu.children && props.menu.children.length > 0;
+});
+
+function navigateTo() {
+    router.push(props.menu.route);
+}
+</script>
+
+<template>
+    <!-- 如果有子選單，使用展開元件 -->
+    <q-expansion-item v-if="hasChildren" :label="menu.label" :icon="menu.icon" expand-separator>
+        <q-list>
+            <MenuItem v-for="child in menu.children" :key="child.key" :menu="child" />
+        </q-list>
+    </q-expansion-item>
+
+    <!-- 無子選單則直接使用 q-item -->
+    <q-item v-else v-ripple clickable @click="navigateTo">
+        <q-item-section avatar>
+            <q-icon :name="menu.icon" />
+        </q-item-section>
+        <q-item-section>
+            {{ menu.label }}
+        </q-item-section>
+    </q-item>
+</template>
