d7fa4734e140462e8fc0b12e7b976d6110b3e2fb
spidergod <spidergod23@gmail.com>
2025-03-14 18:40:44 +0800

    Update router-middleware.global.ts

--- plugins/router-middleware.global.ts
+++ plugins/router-middleware.global.ts
@@ -1,12 +1,12 @@
 import { useRouter } from 'vue-router';
 
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
     const router = useRouter(); // 獲取 router 實例
 
     // 添加一個全局導航守衛
     router.beforeEach((to, from) => {
-        console.log('✨Global Middleware via Plugin:✨');
-        console.log('From:', from.path, 'To:', to.path);
+        console.warn('✨Global Middleware via Plugin:✨', 'From:', from.path, 'To:', to.path);
+        console.log('to', to);
         // 示例：攔截訪問 "/forbidden" 頁面，並跳轉到 "/login"
         if (to.path === '/forbidden') {
             return '/login'; // 返回一個路徑地址
