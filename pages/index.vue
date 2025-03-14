8d988ca79ea5f7595e8d3689e0d2a421d1acbffa
spidergod <spidergod23@gmail.com>
2025-03-14 18:42:49 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <script setup>
 // 單頁應用 redirect 中介層
 definePageMeta({ middleware: 'redirect' });
-
 // 在頁面進入時，自動導航到 /dashboard
 onMounted(() => {
-    navigateTo('/dashboard');
+    // navigateTo('/dashboard');
     console.log('歡迎來到首頁');
 });
 </script>
