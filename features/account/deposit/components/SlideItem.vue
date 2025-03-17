9b75e5d9396759794896c3def8c836ae42b9bc13
spidergod <spidergod23@gmail.com>
2025-03-17 18:26:35 +0800

    Create SlideItem.vue

---
+++ features/account/deposit/components/SlideItem.vue
@@ -0,0 +1,25 @@
+// features/account/components/SlideItem.vue
+// 滑動項目組件
+<script setup lang="ts">
+const { title, amount } = defineProps<{
+    title: string
+    amount: number
+}>();
+</script>
+
+<template>
+    <q-slide-item>
+        <template #default>
+            <q-card flat class="full-width q-pa-md">
+                <div class="row items-center justify-between">
+                    <div class="text-subtitle1">
+                        {{ title }}
+                    </div>
+                    <div class="text-h6">
+                        {{ amount }}
+                    </div>
+                </div>
+            </q-card>
+        </template>
+    </q-slide-item>
+</template>
