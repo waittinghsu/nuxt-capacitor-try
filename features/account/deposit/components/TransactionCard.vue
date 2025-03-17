4d73efd9a72108d8f8289d8792b15fb5d7399b93
spidergod <spidergod23@gmail.com>
2025-03-17 18:26:50 +0800

    Create TransactionCard.vue

---
+++ features/account/deposit/components/TransactionCard.vue
@@ -0,0 +1,29 @@
+// features/account/components/TransactionCard.vue
+// 交易卡片組件
+<script setup lang="ts">
+const props = defineProps<{
+    title: string
+    amount: number
+    description: string
+    progress: number
+}>();
+</script>
+
+<template>
+    <q-card flat class="q-mb-sm">
+        <q-card-section>
+            <div class="row justify-between items-center">
+                <div class="text-subtitle1">
+                    {{ title }}
+                </div>
+                <div class="text-h6">
+                    {{ amount }}
+                </div>
+            </div>
+            <div class="text-caption q-mt-sm" style="-webkit-line-clamp: 2; display: -webkit-box; -webkit-box-orient: vertical; overflow: hidden;">
+                {{ description }}
+            </div>
+            <q-linear-progress class="q-mt-sm" :value="progress" />
+        </q-card-section>
+    </q-card>
+</template>
