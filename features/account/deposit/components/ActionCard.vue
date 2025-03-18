0e7d2828423126a2a7e4f54ff478c2ea3a0d551d
spidergod <spidergod23@gmail.com>
2025-03-18 17:51:47 +0800

    Update ActionCard.vue

--- features/account/deposit/components/ActionCard.vue
+++ features/account/deposit/components/ActionCard.vue
@@ -2,24 +2,24 @@
 // 四個功能方塊的組件
 <script setup lang="ts">
 const actions = [
-    { icon: 'upload', label: '導出' },
-    { icon: 'add_circle', label: '入金' },
-    { icon: 'star', label: '優惠' },
-    { icon: 'favorite', label: '我的最愛' },
+    { icon: 'upload', label: 'Upload' },
+    { icon: 'savings', label: 'Virtual Card' },
+    { icon: 'style', label: '優惠' },
+    { icon: 'diversity_1', label: '我的最愛' },
 ];
 </script>
 
 <template>
-    <div class="row q-col-gutter-sm q-pa-md">
+    <div class="row q-col-gutter-sm q-pa-md bg-deep">
         <div v-for="action in actions" :key="action.label" class="col-3">
-            <q-btn flat class="full-width" style="height: 80px">
+            <q-btn class="full-width shadow-9" color="black" no-caps style="height: 80px; border-radius: 16px;">
                 <div class="column items-center">
-                    <q-icon :name="action.icon" size="24px" />
-                    <div class="q-mt-sm text-caption">
-                        {{ action.label }}
-                    </div>
+                    <q-icon color="purple" :name="action.icon" size="36px" />
                 </div>
             </q-btn>
+            <div class="q-mt-sm text-caption text-center">
+                {{ action.label }}
+            </div>
         </div>
     </div>
 </template>
