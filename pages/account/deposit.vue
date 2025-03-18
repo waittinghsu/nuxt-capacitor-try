3ca48cb23d7035f318dccc6c8f801e24c316be58
spidergod <spidergod23@gmail.com>
2025-03-18 17:55:29 +0800

    Update deposit.vue

--- pages/account/deposit.vue
+++ pages/account/deposit.vue
@@ -1,16 +1,15 @@
 <!-- pages/account/deposit.vue -->
 <script setup lang="ts">
-import ActionGrid from '@/features/account/deposit/components/ActionGrid.vue';
+import type { Card, Transaction } from '@/features/account/deposit/types';
+import ActionGrid from '@/features/account/deposit/components/ActionCard.vue';
 import BalanceDisplay from '@/features/account/deposit/components/BalanceDisplay.vue';
 import SlideItem from '@/features/account/deposit/components/SlideItem.vue';
 import TransactionCard from '@/features/account/deposit/components/TransactionCard.vue';
 import { ref } from 'vue';
 
-const totalBalance = ref(100000);
-const availableBalance = ref(80000);
-const frozenBalance = ref(20000);
+const totalBalance = ref<number>(100000);
 
-const transactions = ref([
+const transactions = ref<Transaction[]>([
     {
         title: '定期存款',
         amount: 50000,
@@ -37,24 +36,40 @@ const transactions = ref([
     },
     // ... 更多交易記錄
 ]);
+const cardList = ref<Card[]>([
+    {
+        name: 'FB',
+        title: 'FuBon Bank',
+        info: 'OMIYAGE',
+    },
+    {
+        name: 'EB',
+        title: 'E.SUN Bank',
+        info: 'StarLux',
+    },
+    {
+        name: 'CB',
+        title: 'Cathay Bank',
+        info: 'Cube',
+    },
+]);
 </script>
 
 <template>
     <q-page>
         <!-- 上半部深色區域 -->
-        <div class="bg-dark">
+        <div class="bg-grey-10">
             <BalanceDisplay :balance="totalBalance" />
 
             <!-- 滑動項目 -->
-            <SlideItem title="Account Detail" :amount="availableBalance" />
-            <SlideItem title="Account Detail" :amount="frozenBalance" />
+            <SlideItem :card-list="cardList" />
 
             <!-- 功能方塊 -->
             <ActionGrid />
         </div>
 
         <!-- 下半部淺色區域 -->
-        <div class="bg-grey-1 q-pa-md">
+        <div class="bg-deep-purple-2 q-pa-md">
             <TransactionCard
                 v-for="(transaction, index) in transactions"
                 :key="index"
