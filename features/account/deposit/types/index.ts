14774e76e0574c8e63866d755030cf8a2aafc8af
spidergod <spidergod23@gmail.com>
2025-03-18 17:50:54 +0800

    Create index.ts

---
+++ features/account/deposit/types/index.ts
@@ -0,0 +1,12 @@
+export interface Card {
+    name: string
+    title: string
+    info: string
+}
+
+export interface Transaction {
+    title: string
+    amount: number
+    description: string
+    progress: number
+}
