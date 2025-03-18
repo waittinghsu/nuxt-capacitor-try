e2b24c6be8b30cc1f6a559e632d90b6bd2b6656a
spidergod <spidergod23@gmail.com>
2025-03-18 18:21:58 +0800

    Update TransactionCard.vue

--- features/account/deposit/components/TransactionCard.vue
+++ features/account/deposit/components/TransactionCard.vue
@@ -1,29 +1,47 @@
 // features/account/components/TransactionCard.vue
 // 交易卡片組件
 <script setup lang="ts">
+import { useQuasar } from 'quasar';
+
 defineProps<{
     title: string
     amount: number
     description: string
     progress: number
 }>();
+const $q = useQuasar();
+const slideItemRef = ref<any>(null); // 用於存儲 q-slide-item 的引用
+
+function handleLeftSlide() {
+    $q.notify('Left action triggered. Resetting in 1 second.');
+    setTimeout(() => {
+        slideItemRef.value?.reset();
+    }, 2000);
+}
 </script>
 
 <template>
-    <q-card flat class="q-mb-sm bg-deep-purple-6">
-        <q-card-section>
-            <div class="row justify-between items-center">
-                <div class="text-subtitle1">
-                    {{ title }}
-                </div>
-                <div class="text-h6">
-                    {{ amount }}
-                </div>
+    <q-slide-item ref="slideItemRef" class="transparent q-mb-sm" left-color="deep-purple-3" @left="handleLeftSlide">
+        <template #left>
+            <div class="row items-center">
+                Right content.. long <q-icon right name="alarm" />
             </div>
-            <div class="text-caption q-mt-sm" style="-webkit-line-clamp: 2; display: -webkit-box; -webkit-box-orient: vertical; overflow: hidden;">
-                {{ description }}
-            </div>
-            <q-linear-progress class="q-mt-sm" :value="progress" />
-        </q-card-section>
-    </q-card>
+        </template>
+        <q-card flat class=" bg-deep-purple-6">
+            <q-card-section>
+                <div class="row justify-between items-center">
+                    <div class="text-subtitle1">
+                        {{ title }}
+                    </div>
+                    <div class="text-h6">
+                        {{ amount }}
+                    </div>
+                </div>
+                <div class="text-caption q-mt-sm" style="-webkit-line-clamp: 2; display: -webkit-box; -webkit-box-orient: vertical; overflow: hidden;">
+                    {{ description }}
+                </div>
+                <q-linear-progress class="q-mt-sm" :value="progress" />
+            </q-card-section>
+        </q-card>
+    </q-slide-item>
 </template>
