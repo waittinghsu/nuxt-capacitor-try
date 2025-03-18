a64c4dd37ab979b7dfab3334a1578aca2caf2461
spidergod <spidergod23@gmail.com>
2025-03-18 17:51:35 +0800

    Update BalanceDisplay.vue

--- features/account/deposit/components/BalanceDisplay.vue
+++ features/account/deposit/components/BalanceDisplay.vue
@@ -1,13 +1,13 @@
 // features/account/components/BalanceDisplay.vue
 // 用於顯示用戶餘額的組件
 <script setup lang="ts">
-const props = defineProps<{
+const { balance } = defineProps<{
     balance: number
 }>();
 </script>
 
 <template>
-    <div class="balance-display q-pa-md text-center bg-dark text-white">
+    <div class="balance-display q-pa-md text-center text-white">
         <div class="text-h6">
             總計餘額
         </div>
